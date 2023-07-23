7f4c560a8540111eae4d9128298125f6af653dec
liushan <shan.liu@msn.com>
2023-07-23 21:04:31 +0800

    反问路径改配置

--- server/examples/lsys-actix-web/src/handler/web/mod.rs
+++ server/examples/lsys-actix-web/src/handler/web/mod.rs
@@ -11,24 +11,35 @@ pub(crate) fn router_ui<T>(app: App<T>, app_dao: &Arc<WebDao>) -> App<T>
 where
     T: ServiceFactory<ServiceRequest, Config = (), Error = Error, InitError = ()>,
 {
-    if let Ok(ui_dir) = app_dao.app_core.config.get_string("ui_dir") {
-        let mut ui_path = PathBuf::from_str(&ui_dir);
-        if let Ok(ref ui_buf) = ui_path {
+    let ui_path = app_dao
+        .app_core
+        .config
+        .get_string("ui_path")
+        .unwrap_or_else(|_| "/ui".to_string());
+    if let Ok(ui_config) = app_dao.app_core.config.get_string("ui_dir") {
+        let mut ui_dir = PathBuf::from_str(&ui_config);
+        if let Ok(ref ui_buf) = ui_dir {
             if !ui_buf.exists() && !ui_buf.is_absolute() {
                 let cargo_dir = env!("CARGO_MANIFEST_DIR");
-                ui_path = PathBuf::from_str(&format!("{}/{}", cargo_dir, ui_dir))
+                ui_dir = PathBuf::from_str(&format!("{}/{}", cargo_dir, ui_config))
             }
         }
-        if let Ok(ui_buf) = ui_path {
+        if let Ok(ui_buf) = ui_dir {
             if ui_buf.exists() {
-                return app.service(web::redirect("/ui", "/ui/")).service(
-                    actix_files::Files::new("/ui/", ui_buf)
-                        .index_file("index.html")
-                        .show_files_listing(),
-                );
+                let ui_path_full = ui_path.trim_matches('/').to_string() + "/";
+                return app
+                    .service(web::redirect(
+                        ui_path.trim_matches('/').to_string(),
+                        ui_path_full.to_owned(),
+                    ))
+                    .service(
+                        actix_files::Files::new(&ui_path_full, ui_buf)
+                            .index_file("index.html")
+                            .show_files_listing(),
+                    );
             }
         }
-        warn!("not find ui dir : {}", ui_dir);
+        warn!("not find ui dir : {}", ui_config);
     } else {
         info!("not set ui dir");
     }
