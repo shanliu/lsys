968985a0f90cfd47f8dad9736ca0298a8d8931aa
liushan <shan.liu@msn.com>
2024-04-13 19:52:19 +0800

    clear

--- server/lsys-app-barcode/src/dao/create.rs
+++
@@ -1,7 +0,0 @@
-use crate::model::BarcodeOutputModel;
-
-use super::BarCodeResult;
-
-pub(crate) fn create_bar_code(_config: &BarcodeOutputModel, _data: &str) -> BarCodeResult<String> {
-    Ok("".to_string())
-}
