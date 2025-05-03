0375f726ce76d5b05cb5b52d875e14de73ed0978
liushan <shan.liu@msn.com>
2025-05-03 16:40:59 +0800

    ..

--- sdk/go/examples/basic/common.go
+++ sdk/go/examples/basic/common.go
@@ -9,12 +9,12 @@ var restApi *lSysApi.RestApi
 func init() {
 	if restApi == nil {
 		restApi = lSysApi.NewRestApi(&lSysApi.RestApiConfig{
-			//应用在 https://www.lsys.site/app.html#/user/app 申请
+			//应用在 https://www.lsys.cc/app.html#/user/app 申请
 			AppId:          "1212f",                            //应用ID
 			AppSecret:      "3f95638a1e07b87df2b64e09c2541dac", //应用Secret
-			AppHost:        "https://www.lsys.site",            //应用HOST
+			AppHost:        "https://www.lsys.cc",            //应用HOST
 			AppOAuthSecret: "2a97bf1b4f075b0ca7467e7c6b223f89", //应用OauthSecret
-			AppOAuthHost:   "https://www.lsys.site/oauth.html",
+			AppOAuthHost:   "https://www.lsys.cc/oauth.html",
 		})
 	}
 }
